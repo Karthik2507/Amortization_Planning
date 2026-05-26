baae0df90ecada5543da6f8b482cdb3912721e34
sathyapanneerselvam1 <sathya.panneerselvam@uvidconsulting.com>
2026-05-26 10:55:44 +0530

    Fourth update

--- Overview.js
+++ Overview.js
@@ -3,7 +3,7 @@
 const newLoanBtn = document.getElementById("newLoanBtn");
 
 newLoanBtn.addEventListener("click", () => {
-    window.location.href = "pages/new-loan.html";
+    window.location.href = "newloan.html";
 });
 
 // RECALCULATE BUTTON
